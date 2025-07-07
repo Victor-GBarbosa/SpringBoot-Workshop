1c7781e71826c242632cdf30e61b6a7007ff01c4
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-07-07 14:03:41 -0300

    SpringBoot Setup

---
+++ src/test/java/curse/java/springbootworkshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package curse.java.springbootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
