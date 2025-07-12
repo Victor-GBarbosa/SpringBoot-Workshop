4c97a71bce6012a68f252816c505e11694275784
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-07-11 23:21:35 -0300

    Update CorsConfig.java

--- src/main/java/curse/java/springbootworkshop/config/CorsConfig.java
+++ src/main/java/curse/java/springbootworkshop/config/CorsConfig.java
@@ -1,4 +1,24 @@
 package curse.java.springbootworkshop.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("prod")
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
