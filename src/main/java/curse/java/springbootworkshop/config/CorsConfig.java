cb7a63955579d362d9d8216460c1df3706998d47
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-07-12 01:12:34 -0300

    varsel api connect

--- src/main/java/curse/java/springbootworkshop/config/CorsConfig.java
+++ src/main/java/curse/java/springbootworkshop/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("http://127.0.0.1:5500", "https://api-front-sage.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
